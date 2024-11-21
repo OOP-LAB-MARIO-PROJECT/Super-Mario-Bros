e22fbacdebe5a362d3a02342a9bbea7bfea9445e
baongoc1104 <166134065+baongoc1104@users.noreply.github.com>
2024-11-21 21:38:07 +0700

    create new branch for scene manager

--- SUPER-MARIO-BROS-OOP/Project1/Utils/SceneManager.cpp
+++ SUPER-MARIO-BROS-OOP/Project1/Utils/SceneManager.cpp
@@ -1,1 +1,63 @@
 #include "SceneManager.h"
+
+// Singleton instance
+SceneManager& SceneManager::getInstance()
+{
+    static SceneManager instance;
+    return instance;
+}
+
+SceneManager::~SceneManager() {}
+
+SceneManager::SceneManager() {}
+
+// Returns the current scene ID
+SceneManager::Scenes SceneManager::getCurrentScene()
+{
+    return currentScene;
+}
+
+// Adds a new scene to the map
+void SceneManager::addScene(Scenes sceneId, unique_ptr<Scene> scene)
+{
+    sceneRegistry[sceneId] = move(scene);
+}
+
+// Deletes a scene from the map
+void SceneManager::deleteScene(Scenes sceneId)
+{
+	sceneRegistry.erase(sceneId);
+}
+
+// Switches to a specified scene
+void SceneManager::navigateTo(Scenes scene)
+{
+    if (sceneRegistry.find(scene) != sceneRegistry.end()) // Check if the scene exists
+    {
+        currentScene = scene;
+    }
+    else
+    {
+        // Handle error (scene not found)
+        cout << "Scene not found: " << static_cast<int>(scene) << endl;
+    }
+}
+
+// Updates the current scene
+void SceneManager::update()
+{
+    if (sceneRegistry.find(currentScene) != sceneRegistry.end())
+    {
+        sceneRegistry[currentScene]->update(0.0f); // Call update for the current scene
+    }
+}
+
+// Renders the current scene
+void SceneManager::render()
+{
+    if (sceneRegistry.find(currentScene) != sceneRegistry.end())
+    {
+        sceneRegistry[currentScene]->display(); // Call display for the current scene
+    }
+}
+
--- SUPER-MARIO-BROS-OOP/Project1/Utils/SceneManager.h
+++ SUPER-MARIO-BROS-OOP/Project1/Utils/SceneManager.h
@@ -1,5 +1,39 @@
 #pragma once
+#include "../Game/Scenes/Scene.h"
+#include <map>
+#include <memory>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
 class SceneManager
 {
+public:
+    enum class Scenes
+    {
+        Game,
+        Exit,
+        Home,
+        Pause,
+    };
+
+    static SceneManager& getInstance();
+
+    Scenes getCurrentScene();
+    void addScene(Scenes sceneId, unique_ptr<Scene> scene);
+	void deleteScene(Scenes sceneId);
+    void navigateTo(Scenes scene);
+    void update();
+    void render();
+    ~SceneManager();
+
+private:
+    map<Scenes, unique_ptr<Scene>> sceneRegistry; // Renamed the map
+    Scenes currentScene = Scenes::Home;   // Default starting scene
+    SceneManager();
+    // Prevent copying and assignment
+    SceneManager(SceneManager const&) = delete;
+    SceneManager& operator=(SceneManager const&) = delete;
 };
 
