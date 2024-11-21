370ccad29b4a722dece415b59423491252edfc5b
Lê Anh Duy <duyla25clc@gmail.com>
2024-11-21 19:46:09 +0700

    Game setup running

--- SUPER-MARIO-BROS-OOP/Project1/Game/Game.h
+++ SUPER-MARIO-BROS-OOP/Project1/Game/Game.h
@@ -1,5 +1,36 @@
 #pragma once
-class Game
-{
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <memory>
+
+
+class Game {
+private:
+	bool m_isRunning = false;
+	sf::Clock m_clock;
+	sf::RenderWindow* window = nullptr;
+
+public:
+
+	static Game& getInstance() {
+		static Game instance;
+		return instance;
+	}
+
+	~Game();
+
+	void start();
+	void run();
+	void exit();
+
+	bool isRunning() const;
+
+private:
+	// private definition for singleton
+	Game() = default;
+	Game(const Game&) = delete;
+	Game& operator=(const Game&) = delete;
 };
 
