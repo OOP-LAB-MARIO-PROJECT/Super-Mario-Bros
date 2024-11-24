99dd2b25f20c65d22cd6f0e1d350d99f26203be8
baongoc1104 <166134065+baongoc1104@users.noreply.github.com>
2024-11-24 16:16:04 +0700

    update Home scene

--- SUPER-MARIO-BROS-OOP/Project1/Game/Scenes/Home/HomeScene.cpp
+++ SUPER-MARIO-BROS-OOP/Project1/Game/Scenes/Home/HomeScene.cpp
@@ -2,12 +2,15 @@
 
 HomeScene::HomeScene(sf::RenderWindow* window) : Scene(window) {
 	Button startGame = Button::createButton(sf::Vector2f(200, 100), sf::Vector2f(300, 250), sf::Color::Yellow, sf::Color::Blue, sf::Color::Green,
-		[]() { std::cout << "Start"; }, "Start", 24, sf::Color::Black);
+		[]() { SceneManager::getInstance().navigateTo(SceneManager::Scenes::Game); }, "Start", 24, sf::Color::Black);
 	buttons.push_back(startGame);
 	
 	Button exit = Button::createButton(sf::Vector2f(200, 100), sf::Vector2f(300, 100), sf::Color::Yellow, sf::Color::Blue, sf::Color::Green,
 		[]() { SceneManager::getInstance().navigateTo(SceneManager::Scenes::Exit); }, "Exit", 24, sf::Color::Black); //thay exit bằng hàm thoát game
 	buttons.push_back(exit);
+
+	Button setting = Button::createButton(sf::Vector2f(200, 100), sf::Vector2f(300, 400), sf::Color::Yellow, sf::Color::Blue, sf::Color::Green,
+		[]() { /*SceneManager::getInstance().navigateTo(SceneManager::Scenes::Setting);*/ std::cout << "setting"; }, "Setting", 24, sf::Color::Black); //thay exit bằng hàm thoát game
 }
 
 
