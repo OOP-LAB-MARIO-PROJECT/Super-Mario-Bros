0dfdfaba5216ab0a5fd7299c7a54d24f8d88489a
baongoc1104 <166134065+baongoc1104@users.noreply.github.com>
2024-11-28 23:45:46 +0700

    clean code a bit

--- SUPER-MARIO-BROS-OOP/Project1/Game/Scenes/Home/HomeScene.h
+++ SUPER-MARIO-BROS-OOP/Project1/Game/Scenes/Home/HomeScene.h
@@ -9,20 +9,7 @@ class HomeScene : public Scene
 	Animation marioAnimation;
     std::shared_ptr<sf::Texture> marioTexture = nullptr;
 	std::shared_ptr<sf::Sprite> marioSprite = nullptr;
-    //sf::Texture hoveredMarioTexture;
-    //sf::Sprite idleMario;
-    //sf::Texture idleMarioTexture;
-	//bool isMarioHovered = false;   
-
-    //used to render mario gif
-	//std::shared_ptr<sf::Texture> marioTexture;
- //   sf::Vector2u imageCount; //to calculate to the size of a single sprite in a row and the number of row
- //   sf::Vector2u currentImage;
- //   float totalTime;    //the total time since we last changed image
- //   float switchTime; //the time it takes before switching to another image
 public:
-    //sf::IntRect rect; //used to display the animation
-
     HomeScene(sf::RenderWindow* window);
     ~HomeScene() {
     };
